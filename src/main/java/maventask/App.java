f3f4de4ee1af4974712302341fd54813832ff4be
root <root@ip-172-31-89-208.ec2.internal>
2019-03-05 09:38:41 +0000

    Revert "program"
    This reverts commit 9b0fb89c1d3a60350b40fe120d911c8d66474b47.

--- src/main/java/maventask/App.java
+++ src/main/java/maventask/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "welcome to mindtree minds" );
+        System.out.println( "Hello World!" );
     }
 }

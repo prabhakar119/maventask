f9b8e2a90b521f96210e5ddd253146ea2b24e909
prabhakar119 <45205129+prabhakar119@users.noreply.github.com>
2019-05-20 10:50:12 +0530

    Update App.java

--- src/main/java/maventask/App.java
+++ src/main/java/maventask/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Dockerizing Jenkins Pipeline" );
+        System.out.println( "Welcome to Hello World" );
     }
 }
